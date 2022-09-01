3ef329f3e44834505c8155075acde7a312e97e7c
gisgiselle <brengimolina@gmail.com>
2022-08-31 21:03:10 -0500

    Add project files.

---
+++ PracticeMVC/App_Start/RouteConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PracticeMVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            /* CUSTOM ROUTE NOT THE BEST USE ATTRIBUTE ROUTING
+            name, custom route, defaults (name of controller and action)
+            to limit to certain values @"a|b"
+            routes.MapRoute(
+                "MoviedByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { controller = "Movies", action= "ByReleaseDate"},
+                new { year = @"\d{4}", month = @"\d{2}" }
+                );
+            */
+
+            //ATTRIBUTE ROUTING
+            routes.MapMvcAttributeRoutes();
+
+            //DEFAULT
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ PracticeMVC/Controllers/MoviesController.cs
@@ -0,0 +1,54 @@
+﻿using PracticeMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticeMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek", Id = 1 };
+                   
+            return View(movie);
+
+
+            //redirect to action(action name, controller name, anonymous object )
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
+        }
+
+        //new url movies/edit/{id}
+        //TIENE QUE LLAMARSE IGUAL QUE LA VARIABLE
+        //FOR CUSTOM ROUTES CHECK ROUTECONFIG
+        public ActionResult Edit(int id)
+        {
+            return Content("id= " + id);
+        }
+        
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            
+                pageIndex = 1;
+            
+            if (String.IsNullOrWhiteSpace(sortBy))
+            
+                sortBy = "Name";
+
+                return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            
+        }
+
+        //ATTRIBUTE ROUTING
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month) 
+        {
+            return Content(year + " " + month);
+        }
+    }
+}
